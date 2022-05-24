78317203a7e5fee76b687f06546ff2fd93ea4df8
VIGNESHWAR-RV-DevShip <vigneshwar@devship.in>
2022-05-24 10:49:55 +0530

    updated with login and reviewing feature

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // const express = require("express");
 
-import express, { response } from "express";
-import {MongoClient} from "mongodb";
+import express from "express";
+import {MongoClient,ObjectId} from "mongodb";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -12,72 +14,6 @@ app.get("/",(request, response)=>{
     response.send("hello, 🌍20000")
 });
 
-const movies = [
-    {
-    "id": "104",
-    "name": "Interstellar",
-    "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-    "rating": 8.6,
-    "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    "trailer": "https://www.youtube.com/embed/zSWdZVtXT7E",
-    "language": "english"
-    },
-    {
-    "id": "100",
-    "name": "Iron man 2",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-    "rating": 7,
-    "summary": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-    "trailer": "https://www.youtube.com/embed/wKtcmiifycU",
-    "language": "english"
-    },
-    {
-    "id": "101",
-    "name": "No Country for Old Men",
-    "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    "rating": 8.1,
-    "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    "trailer": "https://www.youtube.com/embed/38A__WT3-o0",
-    "language": "english"
-    },
-    {
-    "id": "102",
-    "name": "Jai Bhim",
-    "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-    "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-    "rating": 8.8,
-    "trailer": "https://www.youtube.com/embed/nnXpbTFrqXA",
-    "language": "tamil"
-    },
-    {
-    "id": "103",
-    "name": "The Avengers",
-    "rating": 8,
-    "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8",
-    "language": "english"
-    },
-    
-    {
-    "id": "105",
-    "name": "Baahubali",
-    "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-    "rating": 8,
-    "summary": "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    "trailer": "https://www.youtube.com/embed/sOEg_YZQsTI",
-    "language": "telugu"
-    },
-    {
-    "id": "106",
-    "name": "Ratatouille",
-    "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-    "rating": 8,
-    "summary": "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    "trailer": "https://www.youtube.com/embed/NgsQ8mVkN8w",
-    "language": "english"
-    }
-    ]
 
 
 //const MONGO_URL = "mongodb://localhost";//default port
@@ -93,42 +29,247 @@ const client = await createConnection();
 
 app.use(express.json()); //middleWare
 
+app.use(cors());
+
+export const user_auth = (request,response,next)=>{
+    try{
+    if(request.header("user_auth")){
+        const token = request.header("user_auth");
+        jwt.verify(token, process.env.SECRET_KEY,async(err,decoded)=>{
+            if(err || !decoded.id){
+                // console.log(err);
+                return response.status(406).send({message:"unAuthorized Access"});
+            }
+            else{
+                // console.log(decoded.id);
+                let user = await client.db("FirstDB").collection("reviewUsers").findOne({_id:ObjectId(decoded.id)});
+                if(user){
+                    // console.log(user);
+                    request.params.user = user;
+                    next();
+                }
+                else{
+                   return response.status(406).send({message:"unauthorized Access"});
+                }
+            }
+          });
+    }else{
+
+        return response.status(404).send({message:"Invalid request"});
+    }
+}
+    catch(err){
+         console.log(err);
+         return  response.status(406).send({error:err.message})
+    }
+}
+
+  app.post("/login",async(request,response)=>{
+
+      try{
+
+        const {email,googleId,userName,name,imageUrl} = request.body;
+
+        if(userName){
+              const isExistingUser = await client.db("FirstDB")
+                                                 .collection("reviewUsers")
+                                                 .findOne({email,userName});
+
+              if(isExistingUser){
+
+                const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY)
+
+                return response.send({token});
+                    
+              }else{
+
+                return response.status(404).send({message:"no such user exists"});
+              }
+        }
+
+        if(googleId){
+
+              const isExistingUser = await client.db("FirstDB")
+                                                 .collection("reviewUsers")
+                                                 .findOne({email,googleId});
+            
+              if(isExistingUser){
+
+                const token = jwt.sign({ id: isExistingUser._id }, process.env.SECRET_KEY)
+
+                return response.send({token});
+                 
+              }
+              else{
+                    // create an user
+                    let newUserName = "";
+
+                    const result = await client.db("FirstDB")
+                                          .collection("reviewUsers")
+                                          .insertOne({email,userName:newUserName,name,imageUrl,googleId});
+
+                    if(result){
+    
+                        const user = await client.db("FirstDB")
+                                                 .collection("reviewUsers")
+                                                 .findOne({email,name,googleId});
+    
+                        if(user){
+    
+                            const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY)
+    
+                            return response.send({token});
+                             
+                        }
+                        else{
+                            return response.status(400).send({message:"Couldn't process the request , please try again"});
+                        }
+    
+                    }
+                    else{
+                        return response.status(400).send({message:"Couldn't process the request , please try again"});
+                    }
+                
+              }
+        }
+           
+        return response.status(404).send({message:"invalid request"});
+      }
+      catch(e){
+          console.log(e.message);
+          return response.status(500).send({message:"something went wrong , please try again later"});
+      }
+  });
+
+  app.post("/signup",async(request,response)=>{
+     
+       try{
+
+          const {email,name,imageUrl,googleId} = request.body;
+
+          if(email && name && googleId === undefined){
+            
+                const newUserName = name+Date.now().toString().split("").reverse().join("").slice(0,6);
+
+                const result =  await client.db("FirstDB")
+                                            .collection("reviewUsers")
+                                            .insertOne({email,userName:newUserName,name,imageUrl:"",googleId:""});
+
+                if(result){
+
+                    const user = await client.db("FirstDB")
+                                             .collection("reviewUsers")
+                                             .findOne({email,userName:newUserName,name});
+
+                        
+
+                    if(user){
+
+                        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY)
+
+                        return response.send({token});
+    
+                    }
+                    else{
+                        return response.status(400).send({message:"Couldn't process the request , please try again"});
+                    }
+                     
+                }
+                else{
+                    return response.status(400).send({message:"Couldn't process the request , please try again"});
+                }
+          }
+          else if(email,name,imageUrl,googleId){
+               
+               const userName="";
+
+               const result = await client.db("FirstDB")
+                                          .collection("reviewUsers")
+                                          .insertOne({email,userName,name,imageUrl,googleId});
+
+                if(result){
+
+                    const user = await client.db("FirstDB")
+                                             .collection("reviewUsers")
+                                             .findOne({email,name,googleId});
+
+                    if(user){
+
+                        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY)
+
+                        return response.send({token});
+                         
+                    }
+                    else{
+                        return response.status(400).send({message:"Couldn't process the request , please try again"});
+                    }
+
+                }
+                else{
+                    return response.status(400).send({message:"Couldn't process the request , please try again"});
+                }
+                
+             
+          }
+
+       }
+       catch(e){
+           console.log(e.message);
+           return response.status(500).send({message:"something went wrong , please try again later"});
+       }
+
+  });
 
     //GET MOVIE WITH FILTERS IN REQUEST
-  app.get("/movies",async(req,res)=>{
+  app.get("/movies",user_auth,async(req,res)=>{
+
       const queries = req.query;
       if(queries.rating){
           queries.rating = +queries.rating;
       }
-      let filteredMovies = await client.db("firstDB").collection("movies").find(queries).sort({id:1}).toArray();
-  
-      //  if(langiage&& rating){
-      //     filteredMovies = await client.db("firstDB").collection("movies").find({"language":language},{"rating":rating}).toArray();
-      //  }
-      //  else if(language){
-      //      filteredMovies = await client.db("firstDB").collection("movies").find({"language":language}).toArray();
-      //  }
-      //  else if(rating){
-      //      filteredMovies = await client.db("firstDB").collection("movies").find({"rating":+rating}).toArray();
-      //  }
+      let filteredMovies = await client.db("FirstDB").collection("movies").find(queries).sort({id:1}).toArray();
+
        
        res.send(filteredMovies);
   })
 
      //GET MOVIE WITH ID
-  app.get("/movies/:id",async(request,response)=>{
-      console.log(request.params.id);
-      const movie = await client.db("firstDB")
-                          .collection("movies")
-                          .findOne({id : request.params.id});
-      response.send(movie);
+  app.get("/movies/:id",user_auth,async(request,response)=>{
+
+      try{
+          const user = request.params.user; 
+          const id = request.params.id;
+          if(user && id){
+              const movie = await client.db("FirstDB")
+                                  .collection("movies")
+                                  .findOne({id});
+              if(movie){
+                //   console.log(user._id.toString());
+                  const userReview = movie.reviews.filter((review)=>review._id === user._id.toString());
+                //   console.log(userReview);
+                  if(userReview.length > 0){
+                    return  response.send({...movie,userReview:userReview[userReview.length - 1].text});
+                  }else{
+                    return response.send({...movie,userReview:""});
+                  }
+              }
+              else{
+                  return response.status(400).send({message:"error while getting the movie , please try again later"});
+              }
+         }    
+      }
+      catch(e){
+          console.log(e.message);
+          return response.status(500).send({message:"error while getting the movie data , please try again later"})
+      }
   });
 
-     //ADD NEW MOVIE
+
+     //ADD NEW MOVIEs
   app.post("/movies",async(request,response)=>{
      const newMovies = request.body;
      
-     const result = await client.db("firstDB")
+     const result = await client.db("FirstDB")
                                 .collection("movies")
                                 .insertMany(newMovies);
    
@@ -136,11 +277,107 @@ app.use(express.json()); //middleWare
      console.log("addedd");
    });
 
+   app.post("/movie/add",user_auth,async(request,response)=>{
 
+      try{
+        const newMovie = request.body;
+
+        if(newMovie){
+             const result = await client.db("FirstDB")
+                                 .collection("movies")
+                                 .insertOne({...newMovie,reviews:[]});
+ 
+             if(result){
+                  return response.send({message:"succesfully added the movie"});
+             }else{
+                 return response.status(400).send({message:"error while adding a movie, please try again"});
+             }
+        }
+      }catch(e){
+          console.log(e.message);
+          return response.status(500).send({message:"error while adding a movie , please try again later"});
+      }
+
+   });
+
+
+   //EDIT A MOVIE WITH ID
+   app.put(`/movies/:id`,user_auth,async(request,response)=>{
+
+      try{
+        const id = request.params.id;
+        const {user} = request.params;
+        const { userReview } = request.body;
+ 
+         if( user && userReview){
+            
+             const review = {_id:user._id.toString(),userName:user.name,text:userReview}
+             
+             const existingMovie = await client.db("FirstDB")
+                                        .collection("movies")
+                                        .findOne({id:id});
+
+
+             if(existingMovie){
+
+
+                const existingReview = existingMovie.reviews.filter((review) => review._id === user._id.toString())
+
+                // console.log(existingReview);
+                if(existingReview.length > 0){
+
+                         const removed = await client.db("FirstDB")
+                                            .collection("movies")
+                                            .updateOne({id:id},{$pull:{reviews:{_id:user._id.toString(),userName:user.name}}});
+
+                         const added = client.db("FirstDB")
+                                                .collection("movies")
+                                                .updateOne({id:id},{$push:{reviews:review}});
+        
+                        if(removed && added){
+                              return response.send({message:"successfully updated the review"});
+                        }
+                        else{
+                            return response.status(400).send({message:"couldnt update the review,Please try again"});
+                        }
+
+                }else{
+
+                        const result = await client.db("FirstDB")
+                                                   .collection("movies")
+                                                   .updateOne({id:id},{$push:{reviews:review}});
+            
+                        if(result){
+                            return response.send({message:"successfully added the review"});
+                        }else{
+                            return response.status(400).send({message:"couldnt update the review,Please try again"});
+                        }
+
+                }
+
+             }
+
+                
+         }
+      }
+      catch(e){
+          console.log(e.message);
+          return res.status(500).send({message:"error while updating the review"});
+      }
+
+    //    const EditMovie = request.body;
+    //    const result = await client.db("FirstDB")
+    //                             .collection("movies")
+    //                             .updateOne({id:id},{$set:EditMovie});
+    //    response.send(result);
+    //    console.log("edited");
+   });
+
+   
    //DELETE A MOVIE OR MOVIES
    app.delete("/movies/:id",async(req,res)=>{
        const id = req.params.id;
-       const result = await client.db("firstDB").collection("movies").deleteOne({id:id})
+       const result = await client.db("FirstDB").collection("movies").deleteOne({id:id})
        res.send(result);
        console.log("deleted");
    });
@@ -148,21 +385,12 @@ app.use(express.json()); //middleWare
    //DELETE MOVIES
    app.delete("/movies",async(req,res)=>{
     const id = req.params.id;
-    const result = await client.db("firstDB").collection("movies").deleteMany({})
+    const result = await client.db("FirstDB").collection("movies").deleteMany({})
     res.send(result);
     console.log("deleted");
     });
 
-   //EDIT A MOVIE WITH ID
-   app.put(`/movies/:id`,async(request,response)=>{
-       const id = request.params.id;
-       const EditMovie = request.body;
-       const result = await client.db("firstDB")
-                                .collection("movies")
-                                .updateOne({id:id},{$set:EditMovie});
-       response.send(result);
-       console.log("edited");
-   });
+  
 
 app.listen(PORT,()=> console.log("server started at ", PORT));
 
